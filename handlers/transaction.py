a3af83c783af0beeed109f086f9c3a337b907052
Den Hafiz <den.m.hafiz@gmail.com>
2025-09-23 07:27:11 +0800

    Import some libraries

--- handlers/transaction.py
+++ handlers/transaction.py
@@ -1,4 +1,7 @@
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
 from utils.database import save_transaction
+from utils.validators import is_valid_currency
 
 EXPENSE_CATEGORIES = [
     'Food',
