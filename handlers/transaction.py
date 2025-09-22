d2544e95f16d018e16110f101a799fb8d158f140
Den Hafiz <den.m.hafiz@gmail.com>
2025-09-22 23:23:21 +0800

    Added expense and income categories

---
+++ handlers/transaction.py
@@ -0,0 +1,21 @@
+from utils.database import save_transaction
+
+EXPENSE_CATEGORIES = [
+    'Food',
+    'Groceries',
+    'Utilities',
+    'Health/Medical',
+    'Transport',
+    'Savings',
+    'Debt',
+    'Personal',
+    'Other'
+]
+
+INCOME_CATEGORIES = [
+    'Paycheck',
+    'Savings',
+    'Investment',
+    'Other'
+]
+
