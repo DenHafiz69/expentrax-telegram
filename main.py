223d8f7c5f12c6edccdd15419bd61b1f2e087cad
Den Hafiz <den.m.hafiz@gmail.com>
2025-09-22 20:23:34 +0800

    Basic working bot with logging

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 # Import necessary modules
 from dotenv import load_dotenv
 import os
+import logging
 
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Access environment variables
-TELEGRAM_API = os.getenv('TELEGRAM_API')
+BOT_TOKEN = os.getenv('TELEGRAM_API')
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    message = (
+        "Hi"
+    )
+    
+    # If new user, record the telegram id and username into the user table
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=message
+    )
+
+def main() -> None:
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start_command))
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
