9252ca80d6439c7fe79025e6aab077059475ebd2
Den Hafiz <den.m.hafiz@gmail.com>
2025-09-22 22:43:33 +0800

    Init database in the new directory

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 import logging
 
-from database import init_db, save_user, save_transaction
+from utils.database import init_db, save_user, save_transaction
 
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler
