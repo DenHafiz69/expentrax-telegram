f8f4ca3a7a87cbbf7d7f82ea6b7a06bcd39784f8
Den Hafiz <den.m.hafiz@gmail.com>
2025-09-22 22:50:09 +0800

    Check if the user already exist

--- handlers/start.py
+++ handlers/start.py
@@ -1,17 +1,18 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from utils.database import save_user
+from utils.database import save_user, read_user
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
+    if not read_user(update.effective_chat.id): # Check if user exist in db
+        save_user(
+            chat_id=update.effective_chat.id,
+            username=update.effective_chat.username
+        )
+    
     message = (
         "Hi"
-    )
-    
-    save_user(
-        chat_id=update.effective_chat.id,
-        username=update.effective_chat.username
     )
     
     # If new user, record the telegram id and username into the user table
