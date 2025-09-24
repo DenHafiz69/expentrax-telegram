0e6188c35779b294853e68199300ed771715bbeb
Den Hafiz <den.m.hafiz@gmail.com>
2025-09-24 12:50:43 +0800

    Lower case the type of transaction

--- handlers/transaction.py
+++ handlers/transaction.py
@@ -125,7 +125,7 @@ async def category_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -
     # Save transaction to database
     save_transaction(
         user_id=user.id,
-        type_of_transaction=context.user_data['type'],
+        type_of_transaction=context.user_data['type'].lower(),
         amount=context.user_data['amount'],
         category=context.user_data['category'],
         description=context.user_data['description'],
