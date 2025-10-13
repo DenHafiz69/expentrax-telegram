45c2129a2fe2e0117435dd50c17eeee36ba97873
Den Hafiz <den.m.hafiz@gmail.com>
2025-10-13 16:04:11 +0800

    Put validator into misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -1,5 +1,12 @@
 import re
 
+def list_chunker(categories: list, chunk_size: int):
+    '''Convert categories into chunk of categories'''
+    return [
+        categories[i:i + chunk_size]
+        for i in range(0, len(categories), chunk_size)
+    ]
+
 CURRENCY_REGEX = re.compile(r'^\d+(\.\d{1,2})?$')
 
 def is_valid_currency(text: str) -> bool:
